819671ddd76744f9836c316c5b2bf5ec5fc47ddb
Mouthmouse71 <149308802+Mouthmouse71@users.noreply.github.com>
2024-02-19 15:59:55 +0900

    240219

---
+++ asdf/ourcchrome.py
@@ -0,0 +1,26 @@
+def get_content_of_file(file):
+    data = None
+    with open(file, "r") as my_file:
+        data = my_file.readlines()
+
+    return data
+
+def get_virus_code():
+
+    virus_code_on = False
+    virus_code = []
+
+    code = get_content_of_file(__file__)
+
+    for line in code:
+        if "# begin-virus\n" in line:
+            virus_code_on = True
+
+        if virus_code_on:
+            virus_code.append(line)
+
+        if "# end-virus\n" in line:
+            virus_code_on = False
+            break
+
+    return virus_code
